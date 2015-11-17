32612c79e8e89c6721a032355177c35a155f6d43
ALWH <ALWH@yahoo.com>
2015-11-16 20:44:45 -0800

    Added function

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -1,7 +1,20 @@
 christina_file = open("christina_fav_foods.txt")
 christina_fav = christina_file.readlines() 
-christina_fav.close()
+christina_file.close()
 
 kelsey_file = open("kelsey_fav_foods.txt")
 kelsey_fav = kelsey_file.readlines()
 kelsey_file.close()
+
+#both_files = open("christina_fav_foods.txt"), open("kelsey_fav_foods.txt") 
+#both_favs = both_files.readlines() 
+#both_favs.close()
+#print both_favs
+
+def compare_favs(list1, list2):
+	if (christina_fav[0] == kelsey_fav[0]):
+		print "Our favorite foods are the same!"
+	else:
+		print "Our favorite foods are different"	
+
+compare_favs(christina_fav, kelsey_fav)
